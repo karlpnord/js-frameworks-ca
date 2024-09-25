d9123aa7c4c2e6b2db45b65431bbbb7e8bcc606d
karlpn <91100704+karlpn@users.noreply.github.com>
2024-09-26 01:27:24 +0200

    Header component updated

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FaHouse, FaEnvelope } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import DarkModeToggle from './DarkModeToggle';
@@ -7,18 +6,18 @@ const NavList = ({navbarOpen}) => {
   return (
     <ul className={`${navbarOpen ? 'block absolute right-0 top-20' : 'hidden'}`}>
       <li>
-        <Link to='/' className='flex items-center gap-2 border border-gray-200 px-12 py-4 hover:bg-gray-100 transition dark:bg-zinc-800 dark:border-gray-400 dark:hover:bg-zinc-600'>
-          <FaHouse className='text-2xl text-gray-700 dark:text-gray-100'/>
-          <span className='font-semibold text-lg  text-gray-800 font-inter dark:text-gray-100'>Home</span>
+        <Link to='/' className='flex items-center justify-center gap-2 border border-gray-200 px-12 py-4 hover:bg-gray-100 transition dark:bg-zinc-800 dark:border-gray-700 dark:hover:bg-zinc-600'>
+          <FaHouse className='text-xl text-gray-700 dark:text-gray-300'/>
+          <span className='font-medium text-md text-gray-800 font-inter dark:text-gray-300'>Home</span>
         </Link>
       </li>
       <li>
-        <Link to='/contacts' className='flex items-center gap-2 border border-gray-200 px-12 py-4 hover:bg-gray-100 transition dark:bg-zinc-800 dark:hover:bg-zinc-600'>
-          <FaEnvelope className='text-2xl text-gray-700 dark:text-gray-100'/>
-          <span className='font-semibold text-lg  text-gray-800 font-inter dark:text-gray-100'>Contact</span>
+        <Link to='/contacts' className='flex items-center justify-center gap-2 border border-gray-200 px-12 py-4 hover:bg-gray-100 transition dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:border-gray-700'>
+          <FaEnvelope className='text-xl text-gray-700 dark:text-gray-300'/>
+          <span className='font-medium text-md text-gray-800 font-inter dark:text-gray-300'>Contact</span>
         </Link>
       </li>
-      <li className='flex items-center gap-2 border border-gray-200 px-12 py-4 dark:bg-zinc-800 '>
+      <li className='flex items-center justify-center gap-2 border border-gray-200 px-12 py-4 dark:bg-zinc-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-zinc-600 transition'>
         <DarkModeToggle />
       </li>
     </ul>
