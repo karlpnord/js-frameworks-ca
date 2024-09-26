154eb01327659611459aa3f2f1e07d9b6649afd0
karlpn <91100704+karlpn@users.noreply.github.com>
2024-09-26 19:02:03 +0200

    Routing added for Homepage, Checkout and Contact

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -12,7 +12,7 @@ const DarkModeToggle = () => {
     <label className="inline-flex items-center cursor-pointer">
       <input type="checkbox" value={darkmode} className="sr-only peer" onChange={() => darkModeHandler()}/>
       <div className="relative w-9 h-5 bg-gray-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-300 dark:peer-focus:ring-white rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-gray-300"></div>
-      <span className="ms-3 font-medium text-md  text-gray-800 font-inter dark:text-gray-300">{darkmode ? 'Light mode' : 'Dark mode'}</span>
+      <span className="ms-3 font-medium text-md  text-gray-800 font-inter dark:text-gray-300">{darkmode ? 'Dark' : 'Light'}</span>
     </label>
   )
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <Outlet />
       <Footer />
     </>
   )
