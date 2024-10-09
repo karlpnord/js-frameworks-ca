6dd5557918e453e8f14557c4a6548e0e8225e779
karlpn <91100704+karlpn@users.noreply.github.com>
2024-10-09 19:23:00 +0200

    Cart localstorage configured

--- src/services/providers/CartContext.jsx
+++ src/services/providers/CartContext.jsx
@@ -1,4 +1,4 @@
-import { useReducer, createContext } from 'react';
+import { useReducer, createContext, useEffect } from 'react';
 import cartReducer from '../state/cartReducer';
 
 const CartContext = createContext();
@@ -6,7 +6,17 @@ const CartContext = createContext();
 const initialState = { cart: [], totalPrice: 0, totalProducts: 0 };
 
 export const CartProvider = ({ children }) => {
-  const [cart, dispatch] = useReducer(cartReducer, initialState);
+  
+  const getInitialState = () => {
+    const savedCart = localStorage.getItem('cartState');
+    return savedCart ? JSON.parse(savedCart) : initialState;
+  }
+  
+  const [cart, dispatch] = useReducer(cartReducer, getInitialState());
+
+  useEffect(() => {
+    localStorage.setItem('cartState', JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
